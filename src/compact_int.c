0c75537210e7e28ca90d1cd7aa524e04aecef847
Che Hongwei <htc.chehw@gmail.com>
2020-06-21 16:51:33 +0900

    fix bugs: compact_int.c

--- src/compact_int.c
+++ src/compact_int.c
@@ -55,6 +55,8 @@ compact_int_t uint256_to_compact_int(const uint256_t * target)
 	cint.exp = 32 - num_zeros;
 	
 	if((cint.exp + num_bytes) >= 32) num_bytes = 32 - cint.exp; 
+	if((num_zeros + num_bytes) >= 32) num_bytes = 32 - num_zeros;
+	
 	debug_printf("exp: %d (0x%.2x)", (int)cint.exp, (int)cint.exp);
 	
 	memcpy(cint.mantissa, (p_end - num_bytes), num_bytes);
@@ -67,9 +69,11 @@ uint256_t compact_int_to_uint256(compact_int_t * cint)
 
 	int num_bytes = 3;
 	int num_zeros = 32 - cint->exp;
-	debug_printf("num_zeros: %d, num_bytes: %d, exp: 0x%.2x", num_zeros, num_bytes, (int)cint->exp);
-
+	
 	if((cint->exp + num_bytes) >= 32) num_bytes = 32 - cint->exp; 
+	if((num_zeros + num_bytes) >= 32) num_bytes = 32 - num_zeros;
+	debug_printf("num_zeros: %d, num_bytes: %d, exp: 0x%.2x", num_zeros, num_bytes, (int)cint->exp);
+	
 	unsigned char * p = (unsigned char *)&target + cint->exp - num_bytes;
 	
 	memcpy(p, cint->mantissa, num_bytes);
@@ -133,7 +137,14 @@ int main(int argc, char **argv)
 	assert(cint.bits == BITS);
 	
 	
-	printf("== test3: compact_int_to_uint256(cint=0x1e0004cb) ...\n");
+	static const uint32_t BITS_test_3_4 = 0x1e0004cb;
+	static const uint256_t TARGET_test_3_4 = {
+		.val = {
+			[28] = 0xcb,
+			[29] = 0x04,
+		},
+	};
+	printf("== test3: compact_int_to_uint256(cint=0x%.8x) ...\n", BITS_test_3_4);
 	cint.bits = 0;
 	cint.exp = 30;
 	cint.mantissa[0] = 0xcb;
@@ -143,15 +154,45 @@ int main(int argc, char **argv)
 	printf("\tcint    : 0x%.8x\n", cint.bits); 
 	dump_line("\tcintdata:   ", &cint, sizeof(cint));
 	dump_line("\ttarget: ", &target, 32);
-	
-	
-	printf("== test4: uint256_to_cint() ==> cint=0x1e0004cb...\n");
+	assert(0 == memcmp(&target, &TARGET_test_3_4, 32));
+
+	printf("== test4: uint256_to_cint() ==> cint=0x%.8x ...\n", BITS_test_3_4);
 	cint.bits = 0;
 	cint = uint256_to_compact_int(&target);
 	dump_line("\ttarget: ", &target, 32);
 	printf("\tcint    : 0x%.8x\n", cint.bits); 
 	dump_line("\tcintdata:   ", &cint, sizeof(cint));
+	assert(cint.bits == BITS_test_3_4);
 	
+	
+	static const uint32_t BITS_test_5_6 = 0x020004cb;
+	static const uint256_t TARGET_test_5_6 = {
+		.val = {
+			[0] = 0xcb,
+			[1] = 0x04,
+		},
+	};
+	
+	printf("== test5: compact_int_to_uint256(cint=0x%.8x) ...\n", BITS_test_5_6);
+	cint.bits = 0;
+	cint.exp = 2;
+	cint.mantissa[0] = 0xcb;
+	cint.mantissa[1] = 0x04;
+	
+	target = compact_int_to_uint256(&cint);
+	printf("\tcint    : 0x%.8x\n", cint.bits); 
+	dump_line("\tcintdata:   ", &cint, sizeof(cint));
+	dump_line("\ttarget: ", &target, 32);
+	assert(0 == memcmp(&target, &TARGET_test_5_6, 32));
+
+	
+	printf("== test6: uint256_to_cint() ==> cint=0x%.8x...\n", BITS_test_5_6);
+	cint.bits = 0;
+	cint = uint256_to_compact_int(&target);
+	dump_line("\ttarget: ", &target, 32);
+	printf("\tcint    : 0x%.8x\n", cint.bits); 
+	dump_line("\tcintdata:   ", &cint, sizeof(cint));
+	assert(cint.bits == BITS_test_5_6);
 	return 0;
 }
 #endif
