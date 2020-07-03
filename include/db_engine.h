8443479f1aff5f780b5c368343ab7f8b1815b49d
Che Hongwei <htc.chehw@gmail.com>
2020-07-03 17:57:00 +0900

    updating: db_engine.c ...

--- include/db_engine.h
+++ include/db_engine.h
@@ -41,6 +41,12 @@ typedef struct db_record_data
 	int flags;
 }db_record_data_t;
 
+enum db_record_flags
+{
+	db_record_flags_no_dup = 0x01,	// disable dup-insert even if the database allows duplicate-keys
+	db_record_flags_no_overwrite = 0x02, // insertion(s) would be failed if the key already exists in the db.
+};
+
 typedef ssize_t (* db_associate_callback)(struct db_handle * sdb, 
 		const db_record_data_t * key, const db_record_data_t * value, // records in the primary db
 		db_record_data_t ** p_skeys // return key(s) in the secondary db
@@ -52,6 +58,8 @@ typedef struct db_handle
 	
 	void * user_data;
 	unsigned int err_code;
+	
+	enum db_record_flags flags; 
 	
 	int (* open)(struct db_handle * db, 
 		db_engine_txn_t * txn, // nullable 
@@ -69,35 +77,44 @@ typedef struct db_handle
 	 * find_secondary(): 	Use the secondary_key to find the primary_keys and values in the primary_db
 	 *   @return num_records, can be greater than 1 if duplicated key is allowed
 	 */
-	ssize_t (* find)(struct db_handle * db, 
+	ssize_t (* find)(struct db_handle * db, db_engine_txn_t * txn, 
 		const db_record_data_t * key, 
 		db_record_data_t ** p_values);
 
-	ssize_t (* find_secondary)(struct db_handle * secondary_db, 
+	ssize_t (* find_secondary)(struct db_handle * secondary_db, db_engine_txn_t * txn, 
 		const db_record_data_t * skey,		// the key of secondary database
 		db_record_data_t ** p_keys,			// if need return the key of the primary database
 		db_record_data_t ** p_values);
 	
-	int (* insert)(struct db_handle * db, const db_record_data_t * key, const db_record_data_t * value);
-	int (* update)(struct db_handle * db, const db_record_data_t * key, const db_record_data_t * value);
-	int (* del)(struct db_handle * db, const db_record_data_t * key);
+	int (* insert)(struct db_handle * db, db_engine_txn_t * txn, const db_record_data_t * key, const db_record_data_t * value);
+	int (* update)(struct db_handle * db, db_engine_txn_t * txn, const db_record_data_t * key, const db_record_data_t * value);
+	int (* del)(struct db_handle * db, db_engine_txn_t * txn, const db_record_data_t * key);
 
 }db_handle_t;
 db_handle_t * db_handle_init(db_handle_t * db, struct db_engine * engine, void * user_data);
 void db_handle_cleanup(db_handle_t * db);
 
+
+enum db_format_type
+{
+	db_format_type_btree = 0, 
+	db_format_type_hash = 1
+};
+
 typedef struct db_engine
 {
 	void * priv;
 	void * user_data;
-	
-	long refs_count;
 	unsigned int err_code;
 	
 	int (* set_home)(struct db_engine * engine, const char * home_dir);
 	
-	db_handle_t * (* open_db)(struct db_engine * engine, const char * db_name, int db_type, int flags);
-	int (* close_db)(db_handle_t * db);
+	db_handle_t * (* open_db)(struct db_engine * engine, 
+		const char * db_filename, // can be NULL if just keep the db in memory
+		enum db_format_type db_type, 
+		int flags	// if need more control, overload this function, define the meaning of the 'flags' and add corresponding implementations
+	);
+	int (* close_db)(struct db_engine * engine, db_handle_t * db);
 	
 	db_engine_txn_t * (* txn_new)(struct db_engine * engine, struct db_engine_txn * parent_txn);
 	void (* txn_free)(struct db_engine * engine, db_engine_txn_t * txn);
--- src/db_engine.c
+++ src/db_engine.c
@@ -40,12 +40,23 @@
 
 #include "db_engine.h"
 
+static inline DB_ENV * db_engine_get_env(db_engine_t * engine) { return *(DB_ENV **)engine->priv; }
+
+#define db_check_error(ret_code, fmt, ...) do {				\
+		if(ret_code) {										\
+			db_engine_t * engine = db_engine_get();			\
+			DB_ENV * env = db_engine_get_env(engine);		\
+			assert(env);									\
+			env->err(env, ret_code, fmt, ##__VA_ARGS__);	\
+		}													\
+	}while(0)
+
 typedef struct db_engine_private
 {
 	DB_ENV * env;
 	pthread_mutex_t mutex;
 	db_engine_t * engine;
-	
+
 	char home_dir[PATH_MAX];
 	u_int32_t env_flags;
 	
@@ -55,9 +66,11 @@ typedef struct db_engine_private
 	
 	db_handle_t * log_db;
 	char error_desc[4096];	//  last error description
+	
+	long refs_count;
 }db_engine_private_t;
 
-static inline DB_ENV * db_engine_get_env(db_engine_t * engine) { return *(DB_ENV **)engine->priv; }
+
 
 
 /**************************************************
@@ -65,6 +78,7 @@ static inline DB_ENV * db_engine_get_env(db_engine_t * engine) { return *(DB_ENV
  **************************************************/
 static inline DB_TXN * db_txn_get_handle(struct db_engine_txn * txn)
 {
+	if(NULL == txn) return NULL;
 	return txn->priv;
 }
 
@@ -91,12 +105,7 @@ static int txn_commit(struct db_engine_txn * txn, int flags)
 	if(db_txn) {
 		rc = db_txn->commit(db_txn, flags);
 		txn->priv = NULL;
-		if(rc) {
-			DB_ENV * env = db_engine_get_env(txn->engine);
-			assert(env);
-			
-			env->err(env, rc, "%s() failed.", __FUNCTION__);
-		}
+		db_check_error(rc, "%s() failed.", __FUNCTION__);
 	}
 	return rc;
 }
@@ -107,12 +116,7 @@ static int txn_abort(struct db_engine_txn * txn)
 	if(db_txn) {
 		rc = db_txn->abort(db_txn);
 		txn->priv = NULL;
-		if(rc) {
-			DB_ENV * env = db_engine_get_env(txn->engine);
-			assert(env);
-			
-			env->err(env, rc, "%s() failed.", __FUNCTION__);
-		}
+		db_check_error(rc, "%s() failed.", __FUNCTION__);
 	}
 	return rc;
 }
@@ -123,12 +127,7 @@ static int txn_prepare(struct db_engine_txn * txn, unsigned char gid[])
 	DB_TXN * db_txn = txn->priv;
 	if(db_txn) {
 		rc = db_txn->prepare(db_txn, gid);
-		if(rc) {
-			DB_ENV * env = db_engine_get_env(txn->engine);
-			assert(env);
-			
-			env->err(env, rc, "%s() failed.", __FUNCTION__);
-		}
+		db_check_error(rc, "%s() failed.", __FUNCTION__);
 	}
 	return rc;
 }
@@ -140,12 +139,7 @@ static int txn_discard(struct db_engine_txn * txn)
 	if(db_txn) {
 		rc = db_txn->discard(db_txn, 0);
 		txn->priv = NULL;
-		if(rc) {
-			DB_ENV * env = db_engine_get_env(txn->engine);
-			assert(env);
-			
-			env->err(env, rc, "%s() failed.", __FUNCTION__);
-		}
+		db_check_error(rc, "%s() failed.", __FUNCTION__);
 	}
 	return rc;
 }
@@ -156,12 +150,7 @@ static int txn_set_name(struct db_engine_txn * txn, const char * name)
 	DB_TXN * db_txn = txn->priv;
 	if(db_txn) {
 		rc = db_txn->set_name(db_txn, name);
-		if(rc) {
-			DB_ENV * env = db_engine_get_env(txn->engine);
-			assert(env);
-			
-			env->err(env, rc, "%s() failed.", __FUNCTION__);
-		}
+		db_check_error(rc, "%s() failed.", __FUNCTION__);
 	}
 	return rc;
 }
@@ -172,12 +161,7 @@ static const char * txn_get_name(struct db_engine_txn * txn)
 	DB_TXN * db_txn = txn->priv;
 	if(db_txn) {
 		rc = db_txn->get_name(db_txn, &name);
-		if(rc) {
-			DB_ENV * env = db_engine_get_env(txn->engine);
-			assert(env);
-			
-			env->err(env, rc, "%s() failed.", __FUNCTION__);
-		}
+		db_check_error(rc, "%s() failed.", __FUNCTION__);
 	}
 	return name;
 }
@@ -257,11 +241,10 @@ db_private_t * db_private_new(db_handle_t * db)
 void db_private_free(db_private_t * priv)
 {
 	if(NULL == priv) return;
-	pthread_mutex_lock(&priv->mutex);
+	
 	if(priv->dbp) {
 		priv->dbp->close(priv->dbp, 0);
 	}
-	pthread_mutex_unlock(&priv->mutex);
 	
 	pthread_mutex_destroy(&priv->mutex);
 	free(priv);
@@ -287,12 +270,7 @@ static int db_open(struct db_handle * db, db_engine_txn_t * txn, const char * na
 	
 	if(flags <= 0) flags = DB_CREATE | DB_AUTO_COMMIT;
 	rc = dbp->open(dbp, db_txn_get_handle(txn), name, NULL, priv->db_type, flags, 0660);
-	if(rc) {
-		DB_ENV * env = db_engine_get_env(db->engine);
-		assert(env);
-		env->err(env, rc, "%s() failed.\n", __FUNCTION__);
-	}
-	
+	db_check_error(rc, "%s() failed.\n", __FUNCTION__);
 	return rc;
 }
 
@@ -365,7 +343,7 @@ static int secondary_db_get_key(DB * secondary,
 	}
 	
 	free(skeys);
-	return num_keys;
+	return 0;
 }
 
 
@@ -385,12 +363,12 @@ static int db_close(struct db_handle * db)
 {
 	return 0;
 }
-static ssize_t db_find(struct db_handle * db, const db_record_data_t * key, db_record_data_t ** p_values)
+static ssize_t db_find(struct db_handle * db, db_engine_txn_t * _txn, const db_record_data_t * key, db_record_data_t ** p_values)
 {
 	return 0;
 }
 
-static ssize_t db_find_secondary(struct db_handle * secondary_db, 
+static ssize_t db_find_secondary(struct db_handle * secondary_db, db_engine_txn_t * _txn, 
 	const db_record_data_t * skey,		// the key of secondary database
 	db_record_data_t ** p_keys,			// if need return the key of the primary database
 	db_record_data_t ** p_values)
@@ -399,17 +377,39 @@ static ssize_t db_find_secondary(struct db_handle * secondary_db,
 }
 	
 
-static int db_insert(struct db_handle * db, const db_record_data_t * key, const db_record_data_t * value)
+static int db_insert(struct db_handle * db, db_engine_txn_t * _txn, 
+	const db_record_data_t * _key, 
+	const db_record_data_t * _value)
+{
+	DBT key, value;
+	memset(&key, 0, sizeof(key));
+	memset(&value, 0, sizeof(value));
+	
+	key.data = (void *)_key->data;
+	key.size = _key->size;
+	
+	value.data = (void *)_value->data;
+	value.size = _value->size;
+	
+	DB * dbp = db_get_handle(db);
+	assert(dbp);
+	DB_TXN * txn = db_txn_get_handle(_txn);
+	
+	u_int32_t flags = 0;
+	if(db->flags & db_record_flags_no_dup) flags |= DB_NODUPDATA;
+	if(db->flags & db_record_flags_no_overwrite) flags |= DB_NOOVERWRITE;
+	
+	int rc = dbp->put(dbp, txn, &key, &value, flags);
+	db_check_error(rc, "%s() failed.\n", __FUNCTION__);
+	return rc;
+}
+
+static int db_update(struct db_handle * db, db_engine_txn_t * _txn, const db_record_data_t * key, const db_record_data_t * value)
 {
 	return 0;
 }
 
-static int db_update(struct db_handle * db, const db_record_data_t * key, const db_record_data_t * value)
-{
-	return 0;
-}
-
-static int db_del(struct db_handle * db, const db_record_data_t * key)
+static int db_del(struct db_handle * db, db_engine_txn_t * _txn, const db_record_data_t * key)
 {
 	return 0;
 }
@@ -438,6 +438,8 @@ db_handle_t * db_handle_init(db_handle_t * db, db_engine_t * engine, void * user
 }
 void db_handle_cleanup(db_handle_t * db)
 {
+	if(NULL == db) return;
+	db_private_free(db->priv);
 	return;
 }
 
@@ -489,6 +491,8 @@ static db_engine_private_t * db_engine_private_new(db_engine_t * engine)
 	rc = pthread_mutex_init(&priv->mutex, NULL);
 	assert(0 == rc);
 	
+	priv->engine = engine;
+	engine->priv = priv;
 	db_engine_resize(engine, 0);
 	
 	return priv;
@@ -497,20 +501,28 @@ static db_engine_private_t * db_engine_private_new(db_engine_t * engine)
 static void db_engine_private_free(db_engine_private_t * priv)
 {
 	if(NULL == priv) return;
+	fprintf(stderr, "%s(%p)...\n", __FUNCTION__, priv);
 	
-	pthread_mutex_lock(&priv->mutex);
-	for(int i = 0; i < priv->count; ++i)
+	if(priv->env)
 	{
-		db_handle_t * db = priv->databases[i];
-		if(db)
+		for(int i = 0; i < priv->count; ++i)
 		{
-			db_handle_cleanup(db);
-			free(db);
-			priv->databases[i] = NULL;
+			db_handle_t * db = priv->databases[i];
+			if(db)
+			{
+				db_handle_cleanup(db);
+				free(db);
+				priv->databases[i] = NULL;
+			}
 		}
+		priv->env->close(priv->env, 0);
+		priv->env = NULL;
 	}
 	priv->count = 0;
-	pthread_mutex_unlock(&priv->mutex);
+	
+	free(priv->databases);
+	priv->databases = NULL;
+	priv->max_size = 0;
 	
 	pthread_mutex_destroy(&priv->mutex);
 	free(priv);
@@ -518,16 +530,72 @@ static void db_engine_private_free(db_engine_private_t * priv)
 
 static int engine_set_home(struct db_engine * engine, const char * home_dir)
 {
+	int rc = -1;
+	if(NULL == home_dir) home_dir = "./data";
+	db_engine_private_t * priv = engine->priv;
+	DB_ENV * env = priv->env;
+	
+	if(NULL == env)
+	{
+		rc = db_env_create(&priv->env, 0);
+		db_check_error(rc, "%s() failed.", __FUNCTION__);
+		assert(0 == rc);
+		
+		env = priv->env;
+	}
+	
+	rc = env->open(env, home_dir, priv->env_flags, 0);
+	db_check_error(rc, "%s() failed.", __FUNCTION__);
+	assert(0 == rc);
+	
+	strncpy(priv->home_dir, home_dir, sizeof(priv->home_dir));
 	return 0;
 }
 
-static db_handle_t * engine_open_db(struct db_engine * engine, const char * db_name, int db_type, int flags)
+static db_handle_t * engine_open_db(struct db_engine * engine, const char * db_name, enum db_format_type db_type, int flags)
 {
-	return NULL;
+	assert(engine && engine->priv);
+	int rc = -1;
+	db_handle_t * db = db_handle_init(NULL, engine, engine->user_data);
+	assert(db);
+	
+	rc = db->open(db, NULL, db_name, db_type, flags);
+	db_check_error(rc, "%s() failed.", __FUNCTION__);
+	assert(0 == rc);
+	
+	db_engine_private_t * priv = engine->priv;
+	pthread_mutex_lock(&priv->mutex);
+	
+	db_engine_resize(engine, priv->count + 1);
+	priv->databases[priv->count++] = db;
+	
+	pthread_mutex_unlock(&priv->mutex);
+	return db;
 }
-static int engine_close_db(db_handle_t * db)
+
+static int engine_close_db(db_engine_t * engine, db_handle_t * db)
 {
-	return 0;
+	int rc = -1;
+	if(NULL == db) return -1;
+	assert(engine && engine->priv);
+	
+	db_engine_private_t * priv = engine->priv;
+	
+	rc = db->close(db);
+	db_check_error(rc, "%s() failed.", __FUNCTION__);
+	
+	pthread_mutex_lock(&priv->mutex);
+	for(ssize_t i = 0; i < priv->count; ++i)
+	{
+		if(priv->databases[i] == db) {
+			priv->databases[i] = priv->databases[--priv->count];
+			priv->databases[priv->count] = NULL;
+			break;
+		}
+	}
+	
+	pthread_mutex_unlock(&priv->mutex);
+	return rc;
 }
 
 static db_engine_txn_t * engine_txn_new(struct db_engine * engine, struct db_engine_txn * parent_txn)
@@ -538,6 +606,7 @@ static db_engine_txn_t * engine_txn_new(struct db_engine * engine, struct db_eng
 	int rc = txn->begin(txn, parent_txn);
 	if(rc)
 	{
+		db_check_error(rc, "%s() failed.", __FUNCTION__);
 		db_engine_txn_cleanup(txn);
 		free(txn);
 		txn = NULL;
@@ -562,41 +631,176 @@ static db_engine_t g_db_engine[1] = {{
 	.txn_new = engine_txn_new,
 	.txn_free = engine_txn_free,
 }};
-db_engine_t * db_engine_get() { return g_db_engine; }
-#define db_engine_add_ref(engine)  do { ++engine->refs_count; } while(0)
-#define db_engine_unref(engine)		db_engine_cleanup(engine)
+static pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER;
+#define global_lock() 		pthread_mutex_lock(&g_mutex)
+#define global_unlock()		pthread_mutex_unlock(&g_mutex)
 
+db_engine_t * db_engine_get() { return g_db_engine; }
+static inline db_engine_t * db_engine_add_ref(db_engine_t * engine)  { 
+	assert(engine);
+	
+	global_lock();
+	db_engine_private_t * priv = engine->priv;
+	if(NULL == priv) // the engine has already been destroyed
+	{
+		global_unlock();
+		return NULL;
+	}
+
+	++priv->refs_count; 
+	global_unlock();
+	return engine;
+} 
+#define db_engine_unref(engine)		db_engine_cleanup(engine)
 
 db_engine_t * db_engine_init(const char * home_dir, void * user_data)
 {
 	db_engine_t * engine = g_db_engine;
 	engine->user_data = user_data;
-
 	db_engine_private_t * priv = db_engine_private_new(engine);
 	assert(priv && engine->priv == priv);
-
-	if(home_dir) engine->set_home(engine, home_dir);
+	engine->set_home(engine, home_dir);
+	
+	db_engine_add_ref(engine);
 	return engine;
 }
 
 void db_engine_cleanup(db_engine_t * engine)
 {
-	if(0 == engine->refs_count) return;
+	if(NULL == engine || NULL == engine->priv) return;
 	
-	if(--engine->refs_count == 0)
+	global_lock();
+	db_engine_private_t * priv = engine->priv;
+	if(NULL == priv || 0 == priv->refs_count) {
+		global_unlock();
+		return;
+	}
+	
+	if(--priv->refs_count == 0)
 	{
 		db_engine_private_free(engine->priv);
 		engine->priv = NULL;
-		engine->user_data = NULL;
 	}
-	
+	global_unlock();
 	return;
 }
 
 #if defined(_TEST_DB_ENGINE) && defined(_STAND_ALONE)
+
+#include "satoshi-types.h"
+struct db_record_block_data
+{
+	// block info
+	struct satoshi_block_header hdr;
+	int32_t txn_count;
+	int32_t height;			// index of the secondary_db
+	
+	// block.dat file info
+	int64_t file_index;
+	int64_t start_pos;		// the begining of the block_data (just after block_file_hdr{magic, size} )
+	
+	// used to verify block_file_hdr : assert( start_pos >= 8 && (*(uint32_t *)(start-8) == magic)  && (*(uint32_t *)(start-4) == block_size) );
+	uint32_t magic;
+	uint32_t block_size;
+}__attribute__((packed));
+
+static ssize_t associate_blocks_height(db_handle_t * db, 
+	const db_record_data_t * key, 
+	const db_record_data_t * value, 
+	db_record_data_t ** p_result)
+{
+	static const int num_results = 1;
+	if(NULL == p_result) return num_results;	// returns the required array size for custom memory allocator
+	
+	db_record_data_t * result = * p_result;
+	if(NULL == result) {
+		result = calloc(1, sizeof(*result));
+		*p_result = result;
+	}
+	
+	struct db_record_block_data * block = (struct db_record_block_data *)value->data;
+	assert(sizeof(*block) == value->size);
+	
+	result->data = &block->height;
+	result->size = sizeof(block->height);
+	return num_results;
+}
+
 int main(int argc, char **argv)
 {
+	db_engine_t * engine = db_engine_init("data", NULL);
+	assert(engine);
 	
+	// create primary_db and secondary_db
+	db_handle_t * sdb = engine->open_db(engine, "blocks_height.db", db_format_type_btree, 0);
+	db_handle_t * db = engine->open_db(engine, "blocks.db", db_format_type_btree, 0);
+	assert(sdb && db);
+	int rc = db->associate(db, NULL, sdb, associate_blocks_height);
+	assert(0 == rc);
+	
+	// add records 
+	unsigned char hash[32] = { 0 };
+	struct db_record_block_data block[1];
+	
+	db->flags = db_record_flags_no_overwrite;
+	
+	for(int i = 0; i < 10; ++i)
+	{	
+		*(int *)hash = 1000 + i;
+		memset(block, 0, sizeof(block));
+		block->height = i;
+		
+		rc = db->insert(db, NULL,
+			&(db_record_data_t){.data = hash, .size = sizeof(hash) },
+			&(db_record_data_t){.data = block, .size = sizeof(block)}
+		);
+		if(rc == DB_KEYEXIST) break;
+		assert(0 == rc);
+	}
+	
+	// dump records
+	DBC * cursor = NULL;
+	DB * dbp = db_get_handle(db);
+	DB * sdbp = db_get_handle(sdb);
+	
+	assert(dbp && sdbp);
+	rc = dbp->cursor(dbp, NULL, &cursor, DB_READ_COMMITTED);
+	db_check_error(rc, "db->cursor() failed");
+	assert(0 == rc);
+	
+	DBT key, value;
+	memset(&key, 0, sizeof(key));
+	memset(&value, 0, sizeof(value));
+	
+	memset(hash, 0, sizeof(hash));
+	key.flags = DB_DBT_USERMEM;
+	key.data = hash;
+	key.ulen = 32;
+	
+	
+	rc = cursor->get(cursor, &key, &value, DB_FIRST);
+	assert(0 == rc);
+	
+	while(0 == rc)
+	{
+		struct db_record_block_data * data = value.data;
+		assert(data && value.size == sizeof(*data));
+		
+		printf("key: %d, value: height=%d\n", *(int *)hash, data->height);
+		rc = cursor->get(cursor, &key, &value, DB_NEXT);
+		db_check_error(rc, "cursor->get()\n");
+	}
+	
+	assert(rc == DB_NOTFOUND);
+	cursor->close(cursor);
+	
+	
+	
+// test add_ref / unref
+	db_engine_add_ref(engine);
+	db_engine_cleanup(engine);
+	db_engine_cleanup(engine);
+	db_engine_cleanup(engine);
 	return 0;
 }
 #endif
